2399dae5e589773bfd09955fbb01c5c4aa25d151
Dharmandar <Dharmandar.kumar@dipoletechi.com>
2023-05-15 19:31:32 +0530

    added serial port data from com

--- src/windows/usb.js
+++ src/windows/usb.js
@@ -1,4 +1,5 @@
 const { WebUSB, usb } = require('usb');
+const { SerialPort, SerialPortMock } = require('serialport')
 
 const usbDevices = async (callback) => {
   const customWebUSB = new WebUSB({
@@ -12,7 +13,32 @@ const usbDevices = async (callback) => {
   return devices;
 };
 
+const getAllSerialPort=async (callback)=>{
+  const serial = await SerialPort.list();
+  if(callback){
+    callback(serial);
+  }
+}
+const getSerialPortData = async (port,callback) => {
+  SerialPortMock.binding
+  SerialPortMock.binding.createPort(port)
+   port = new SerialPortMock({ path: port, baudRate: 9600 })
+
+   port.on('open', () => {
+    setInterval(()=>{
+      port.on('data', function(chunk) {
+        if(callback){
+          callback(chunk);
+        }
+      });
+    },1000);
+   })
+  
+}
+
 module.exports = {
   usbDevices,
-  useEvent:usb
+  useEvent:usb,
+  getSerialPortData,
+  getAllSerialPort
 }
